6c07516b3dd999fc019159d0c40f219c1f4d0166
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2019-01-21 13:51:42 +0100

    app added

--- src/main/java/se/lexicon/erik/git_test/App.java
+++ src/main/java/se/lexicon/erik/git_test/App.java
@@ -9,7 +9,7 @@ public class App
         numbers.setNumber1(1.0);
         numbers.setNumber2(3.0);
         
-        System.out.println(add(numbers.getNumber1(), numbers.getNumber2()));
+        System.out.println("sum: " + add(numbers.getNumber1(), numbers.getNumber2()));
     }
     
     public static double add(double number1, double number2) {

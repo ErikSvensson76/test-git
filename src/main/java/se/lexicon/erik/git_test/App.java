483caf92cfbb80322f869d1c6b4ccc2ccf7d7d6a
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2019-01-21 13:44:28 +0100

    implemented add.

--- src/main/java/se/lexicon/erik/git_test/App.java
+++ src/main/java/se/lexicon/erik/git_test/App.java
@@ -5,10 +5,14 @@ public class App
 {
     public static void main( String[] args )
     {
+        Foo numbers = new Foo();
+        numbers.setNumber1(1.0);
+        numbers.setNumber2(3.0);
         
+        System.out.println(add(numbers.getNumber1(), numbers.getNumber2()));
     }
     
     public static double add(double number1, double number2) {
-    	return 0;
+    	return number1 + number2;
     }
 }

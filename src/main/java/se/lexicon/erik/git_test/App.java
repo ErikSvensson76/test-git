a3ac7fc954875e2eebc35d0ca66204ade9cee0b2
Erik Svensson <eosvensson@gmail.com>
2019-01-21 13:29:36 +0100

    First commit

---
+++ src/main/java/se/lexicon/erik/git_test/App.java
@@ -0,0 +1,14 @@
+package se.lexicon.erik.git_test;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+    
+    public static double add(double number1, double number2) {
+    	return 0;
+    }
+}
---
+++ src/main/java/se/lexicon/erik/git_test/Foo.java
@@ -0,0 +1,5 @@
+package se.lexicon.erik.git_test;
+
+public class Foo {
+	
+}
